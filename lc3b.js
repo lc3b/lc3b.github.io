a434b582ad8e06a545f6ab95b3a3fc504c6692d3
xrl <693059+xrl@users.noreply.github.com>
2026-01-18 02:13:45 +0000

    Automated Commit

--- lc3b.js
+++ lc3b.js
@@ -138,6 +138,32 @@ export class WasmComputer {
 if (Symbol.dispose) WasmComputer.prototype[Symbol.dispose] = WasmComputer.prototype.free;
 
 /**
+ * Compile C source code to LC-3b assembly
+ * @param {string} source
+ * @returns {string}
+ */
+export function compile_c_to_assembly(source) {
+    let deferred3_0;
+    let deferred3_1;
+    try {
+        const ptr0 = passStringToWasm0(source, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        const len0 = WASM_VECTOR_LEN;
+        const ret = wasm.compile_c_to_assembly(ptr0, len0);
+        var ptr2 = ret[0];
+        var len2 = ret[1];
+        if (ret[3]) {
+            ptr2 = 0; len2 = 0;
+            throw takeFromExternrefTable0(ret[2]);
+        }
+        deferred3_0 = ptr2;
+        deferred3_1 = len2;
+        return getStringFromWasm0(ptr2, len2);
+    } finally {
+        wasm.__wbindgen_free(deferred3_0, deferred3_1, 1);
+    }
+}
+
+/**
  * @param {string} program
  */
 export function parse_program(program) {
